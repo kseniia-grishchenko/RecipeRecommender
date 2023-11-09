b605c8a384baef637916a647cdabe36bb1d7daef
kseniia <kseniia@smartiot.cloud>
2023-11-09 21:32:19 +0100

    header comp

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,21 @@
-<template></template>
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <header-comp></header-comp>
+      </el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
+</template>
 
 <script setup>
-const test = 'a';
+import HeaderComp from './components/HeaderComp.vue';
 </script>
 
-<style scoped></style>
+<style scoped>
+.el-header {
+  border-top: 12px solid var(--main-bg);
+}
+</style>
---
+++ frontend/src/components/HeaderComp.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="header">
+    <div class="logo">
+      <el-icon><CoffeeCup /></el-icon>
+      <span>RecipeRec</span>
+    </div>
+    <div class="sections">
+      <el-link>Home</el-link>
+      <el-link>Recipes</el-link>
+      <el-link>Profile</el-link>
+    </div>
+    <div class="action-section">
+      <el-icon><Star /></el-icon>
+      <el-input
+        v-model="input2"
+        class="search-bar"
+        size="medium"
+        placeholder="Search..."
+        :prefix-icon="Search"
+      />
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped>
+.header {
+  display: grid;
+  grid-auto-flow: column;
+  align-items: center;
+  padding-top: 10px;
+}
+
+.logo {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  margin-right: 30px;
+}
+
+.sections {
+  display: flex;
+  gap: 20px;
+}
+
+.action-section {
+  align-self: flex-end;
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  gap: 6px;
+}
+
+.search-bar {
+  width: 200px;
+}
+</style>
