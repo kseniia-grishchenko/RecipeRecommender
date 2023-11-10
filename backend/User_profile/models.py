bb2c05c3380f9b8d79936e22466c16d798f4fc64
Diana Vyshnivska <65354085+DianaVish@users.noreply.github.com>
2023-11-10 18:31:17 +0200

    models.py

--- backend/User_profile/models.py
+++ backend/User_profile/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
-    # Додайте інші поля за необхідності
+   
