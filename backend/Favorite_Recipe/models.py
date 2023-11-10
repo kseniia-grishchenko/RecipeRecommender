6982b0ad74526010cab44556eae81a9986a41483
Diana Vyshnivska <65354085+DianaVish@users.noreply.github.com>
2023-11-10 18:36:19 +0200

    models.py

--- backend/Favorite_Recipe/models.py
+++ backend/Favorite_Recipe/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class FavoriteRecipe(models.Model):
     user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    # Додайте інші поля за необхідності
+    
 
