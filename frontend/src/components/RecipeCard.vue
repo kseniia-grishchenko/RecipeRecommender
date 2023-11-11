16c9027f035988dcae20cd383a1859e1ae825ab6
kseniia <kseniia@smartiot.cloud>
2023-11-11 20:22:35 +0100

    RecipeCard/List components

---
+++ frontend/src/components/RecipeCard.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="recipe-card">
+    <div class="image-container">
+      <img :src="recipe.image" alt="Recipe image" />
+      <button class="favorite-button">❤️</button>
+    </div>
+    <div class="content">
+      <h2 class="title">{{ recipe.title }}</h2>
+      <p class="description">{{ recipe.description }}</p>
+      <button class="more-button">More...</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    recipe: {
+      type: Object,
+      required: true,
+      default: () => ({
+        image: '',
+        title: 'Title',
+        description: 'Description of the recipe'
+      })
+    }
+  }
+};
+</script>
+
+<style scoped>
+.recipe-card {
+  max-width: 300px;
+  border-radius: 10px;
+  overflow: hidden;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  background-color: #fff;
+  margin: 10px;
+}
+
+.image-container {
+  position: relative;
+}
+
+.favorite-button {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background: none;
+  border: none;
+  font-size: 24px;
+  cursor: pointer;
+}
+
+img {
+  width: 100%;
+  display: block;
+}
+
+.content {
+  padding: 15px;
+}
+
+.title {
+  font-size: 18px;
+  margin: 0 0 10px 0;
+}
+
+.description {
+  font-size: 14px;
+  margin: 0 0 15px 0;
+  color: #666;
+}
+
+.more-button {
+  background-color: var(--main-bg);
+  border: none;
+  padding: 10px 20px;
+  border-radius: 20px;
+  cursor: pointer;
+  font-weight: bold;
+}
+</style>
---
+++ frontend/src/views/RecipeList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="recipe-list" v-if="active">
+    <RecipeCard v-for="recipe in recipes" :key="recipe.id" :recipe="recipe" />
+  </div>
+</template>
+
+<script>
+import recipes from '../assets/data/recipes.js';
+import RecipeCard from '../components/RecipeCard.vue';
+
+export default {
+  data: () => ({
+    recipes,
+    active: false
+  }),
+  methods: {
+    hashChangeHandler() {
+      this.active = location.hash.match('recipes');
+    }
+  },
+  created() {
+    window.addEventListener('hashchange', this.hashChangeHandler);
+    this.hashChangeHandler();
+  },
+  components: {
+    RecipeCard
+  }
+};
+</script>
+
+<style scoped>
+.recipe-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+  padding: 20px;
+}
+</style>
