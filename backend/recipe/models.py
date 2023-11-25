dd5400d4236f20200a90ca2138644822afa0c265
Nataliia <dominok56@gmail.com>
2023-11-26 01:49:46 +0200

    added serializers, views and urls for  recipes

--- backend/recipe/models.py
+++ backend/recipe/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 class Recipe(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
