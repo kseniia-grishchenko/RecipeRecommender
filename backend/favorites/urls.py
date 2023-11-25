32f4d325bdd10c5d81a1fe289fcf2d79bcb36a91
Nataliia <dominok56@gmail.com>
2023-11-26 01:46:39 +0200

    added serializers, views and urls for favourite recipes

--- backend/favorites/urls.py
+++ backend/favorites/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import favorite_recipes, add_to_favorites
+from .views import FavoriteRecipeListCreateView, UserFavoriteRecipeListView
 
 
 urlpatterns = [
-    path('favorites/', favorite_recipes, name='favorite_recipes'),
-    path('add_to_favorites/<int:recipe_id>/', add_to_favorites, name='add_to_favorites'),
+    path('favorite-recipes/', FavoriteRecipeListCreateView.as_view(), name='favorite-recipe-list-create'),
+    path('user-favorite-recipes/<int:user_id>/', UserFavoriteRecipeListView.as_view(), name='user-favorite-recipe-list'),
 ]
