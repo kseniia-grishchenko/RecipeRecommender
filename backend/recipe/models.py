c3be14f8841505c294c0c69fa0bb1a5406d1903a
Nataliia <dominok56@gmail.com>
2023-11-26 11:04:53 +0200

    recipe has ingredients as n-to-n field

--- backend/recipe/models.py
+++ backend/recipe/models.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from ingredient.models import Ingredient
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    ingredients = models.TextField()
+    ingredients = models.ManyToManyField(Ingredient)
     instructions = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
