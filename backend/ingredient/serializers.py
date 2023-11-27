d855510938f38d4e0a214240237287a54bc77a6d
Nataliia <dominok56@gmail.com>
2023-11-27 21:13:01 +0200

    added most popular recipes endpoint

--- backend/ingredient/serializers.py
+++ backend/ingredient/serializers.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from .models import Ingredient
 
 
+class IngredientNameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ('name',)
+
+
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
--- backend/recipe/models.py
+++ backend/recipe/models.py
@@ -13,3 +13,9 @@ class Recipe(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+
+class RecipeUserView(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    time = models.DateField(auto_now_add=True)
