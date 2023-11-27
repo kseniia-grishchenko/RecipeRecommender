4ee981751ce7ac0eb8a2b22e14e1978fe284b872
kseniia <kseniia@smartiot.cloud>
2023-11-27 20:26:44 +0100

    use /auth/users endpoint

--- frontend/src/views/SignUp.vue
+++ frontend/src/views/SignUp.vue
@@ -4,30 +4,42 @@
       <h2>Welcome to RecipeRec</h2>
       <el-form status-icon label-width="120px" class="user-info">
         <el-form-item label="Username" prop="username">
-          <el-input placeholder="Username" />
+          <el-input placeholder="Username" v-model="form.username" />
         </el-form-item>
         <el-form-item label="Password" prop="pass">
-          <el-input type="password" autocomplete="off" placeholder="Password" />
+          <el-input
+            type="password"
+            autocomplete="off"
+            placeholder="Password"
+            v-model="form.password"
+          />
         </el-form-item>
         <el-form-item label="Email" prop="email">
-          <el-input type="email" placeholder="Email" />
+          <el-input type="email" placeholder="Email" v-model="form.email" />
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" class="sign-up-btn">Sign Up</el-button>
+          <el-button type="primary" class="sign-up-btn" @click="submitForm">Sign Up</el-button>
         </el-form-item>
       </el-form>
       <div class="additional">
         <span>Already have an account? </span>
-        <el-link href="/sign-up">Sign in</el-link>
+        <el-link href="/#/sign-in">Sign in</el-link>
       </div>
     </el-main>
   </el-container>
 </template>
 
 <script>
+import { postRequestUnauthorized } from '../api.js';
+
 export default {
   data: () => ({
-    active: false
+    active: false,
+    form: {
+      email: '',
+      username: '',
+      password: ''
+    }
   }),
   props: {
     loggedIn: {
@@ -36,6 +48,29 @@ export default {
     }
   },
   methods: {
+    async submitForm() {
+      try {
+        await postRequestUnauthorized('/api/auth/users/', {
+          username: this.form.username,
+          email: this.form.email,
+          password: this.form.password
+        });
+
+        this.$notify({
+          title: 'Success',
+          message: 'Account created',
+          type: 'success',
+          showClose: false
+        });
+        location.hash = '#/sign-in';
+      } catch (err) {
+        this.$notify.error({
+          title: 'Error occurred',
+          message: JSON.stringify(err.response.data),
+          showClose: false
+        });
+      }
+    },
     hashChangeHandler() {
       this.active = location.hash.match('sign-up');
     }
