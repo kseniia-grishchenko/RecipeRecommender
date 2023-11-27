7aae91b4504530871a1ddc1d0919d4839e589593
Nataliia <dominok56@gmail.com>
2023-11-27 21:13:01 +0200

    added most popular recipes endpoint

--- backend/recipe/urls.py
+++ backend/recipe/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RecipeListCreateView, RecipeRetrieveUpdateDestroyView
+from .views import RecipeListCreateView, RecipeRetrieveUpdateDestroyView, recipe_views_count
 
 
 urlpatterns = [
     path('', RecipeListCreateView.as_view(), name='recipe-list-create'),
     path('<int:pk>/', RecipeRetrieveUpdateDestroyView.as_view(), name='recipe-retrieve-update-destroy'),
+    path('popular-recipes/', recipe_views_count, name='popular-recipes'),
 ]
--- backend/recipe/views.py
+++ backend/recipe/views.py
@@ -1,5 +1,11 @@
+from django.db.models import Count
 from rest_framework import generics, permissions
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 from .models import Recipe
+from .models import RecipeUserView
 from .serializers import RecipeSerializer
 
 
@@ -15,3 +21,26 @@ class RecipeListCreateView(generics.ListCreateAPIView):
 class RecipeRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
+
+    def get(self, request, *args, **kwargs):
+        recipe = self.get_object()
+        RecipeUserView.objects.create(recipe=recipe, user=self.request.user)
+        return super().get(request, *args, **kwargs)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def recipe_views_count(request):
+    recipe_views_counter = RecipeUserView.objects.values('recipe').annotate(views_count=Count('recipe'))
+    sorted_recipe_views_count = sorted(recipe_views_counter, key=lambda x: x['views_count'], reverse=True)
+
+    result = []
+    for entry in sorted_recipe_views_count:
+        recipe_id = entry['recipe']
+        views_count = entry['views_count']
+        recipe = Recipe.objects.get(pk=recipe_id)
+
+        recipe_data = RecipeSerializer(recipe).data
+        result.append({'recipe': recipe_data, 'views_count': views_count})
+
+    return Response(result)
