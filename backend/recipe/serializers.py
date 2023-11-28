ea4d8a7f47882840022b1341ae11dd94db71e277
Nataliia <dominok56@gmail.com>
2023-11-28 18:07:33 +0200

    fixed serializer

--- backend/recipe/serializers.py
+++ backend/recipe/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Recipe
-from ingredient.serializers import IngredientNameSerializer
+from ingredient.serializers import IngredientNameSerializer, Ingredient
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -17,3 +17,13 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         exclude = ('author', )
+
+    def create(self, validated_data):
+        ingredients_data = validated_data.pop('ingredients', [])
+        recipe = Recipe.objects.create(**validated_data)
+
+        for ingredient_data in ingredients_data:
+            ingredient, created = Ingredient.objects.get_or_create(**ingredient_data)
+            recipe.ingredients.add(ingredient)
+
+        return recipe
